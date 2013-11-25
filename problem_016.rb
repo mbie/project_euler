897d8169d9af56a62bd4ba378cf552cfcc0cc95e
mbie <mbielec@pgs-soft.com>
2013-11-25 09:15:07 +0100

    Added solution for problem 16

---
+++ problem_016.rb
@@ -0,0 +1,13 @@
+# Power digit sum
+# Problem 16
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 2^1000?
+
+def power_digit_sum(power)
+  result = 2 ** power
+  result.to_s.chars.map(&:to_i).inject(&:+)
+end
+
+puts power_digit_sum(15)    # => 26
+puts power_digit_sum(1000)  # => 1366
